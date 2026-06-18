688aee080f278f8bfecc9df7dab56d5d11382db9
Kamil Owczarek <milkaowca@gmail.com>
2026-06-18 22:00:27 +0200

    feat: implement persistent search params during navigation

--- src/app/[code]/layout.tsx
+++ src/app/[code]/layout.tsx
@@ -1,4 +1,4 @@
-import GoHomeLink from "@/components/go-home-link";
+import GoBackButton from "@/components/go-back-button";
 import React from "react";
 
 export default function CountryPageLayout({
@@ -9,7 +9,7 @@ export default function CountryPageLayout({
   return (
     <main className="flex flex-1 px-7 pt-10 pb-14 md:px-25 md:pb-16 xl:p-20">
       <div className="mx-auto flex max-w-80 flex-1 flex-col gap-16 md:max-w-xl md:gap-14 xl:max-w-7xl xl:gap-20">
-        <GoHomeLink />
+        <GoBackButton />
         {children}
       </div>
     </main>
--- src/components/go-back-button.tsx
+++ src/components/go-back-button.tsx
@@ -1,13 +1,19 @@
-import { IoArrowBackOutline } from "react-icons/io5";
-import Link from "next/link";
+"use client";
 
-export default function GoHomeLink() {
+import { IoArrowBackOutline } from "react-icons/io5";
+import { useRouter } from "next/navigation";
+
+export default function GoBackButton() {
+  const router = useRouter();
+
+  const handleBack = () => router.back();
+
   return (
-    <Link
-      href={"/"}
+    <button
+      onClick={handleBack}
       className="flex min-h-8 w-fit items-center gap-2 rounded-xs bg-card px-6 text-sm/[2.25] font-semibold text-ink-primary shadow-card transition-colors hover:bg-ink-primary/5 focus-visible:outline-offset-4 md:min-h-10 md:px-8 md:text-base/loose">
       <IoArrowBackOutline className="size-4" />
       <span>Back</span>
-    </Link>
+    </button>
   );
 }
